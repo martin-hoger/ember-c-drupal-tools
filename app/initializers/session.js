d5f33531681a4a6e761410afaf7d0a93f6e3d980
Lukáš Höger <lukas.hoger@gmail.com>
2018-01-23 13:07:02 +0100

    Added initialization of session for helpers.

--- app/initializers/session.js
+++ app/initializers/session.js
@@ -2,6 +2,7 @@ export function initialize(application) {
   application.inject('route', 'session', 'service:session');
   application.inject('controller', 'session', 'service:session');
   application.inject('component', 'session', 'service:session');
+  application.inject('helper', 'session', 'service:session');
 }
 
 export default {
