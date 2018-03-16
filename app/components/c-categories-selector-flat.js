f5b49b700e7099523311ba843f39acd1751cbcba
Martin Höger <martin.hoger@gmail.com>
2018-03-16 15:11:39 +0100

    New category selector

--- app/components/c-categories-selector-flat.js
+++ app/components/c-categories-selector-flat.js
@@ -1,5 +1,30 @@
-import Ember from 'ember';
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { sortCategoriesByStructure } from 'ember-c-drupal-tools/helpers/sort-categories-by-structure';
 
-export default Ember.Component.extend({
-    tagName: '',
+export default Component.extend({
+  tagName: '',
+  searchField: 'fullName',
+  sortByStructure: true,
+  onlyChildren: false,
+
+  filteredOptions: computed('options', function () {
+    var options = this.get('options');
+
+    // If we should keep only children.
+    if (this.get('onlyChildren')) {
+      options = options.filter(function (category) {
+        return category.get('depth') > 0;
+      });
+      this.set('sortByStructure', false);
+    }
+
+    // Sort by structure.
+    if (this.get('sortByStructure')) {
+      options = sortCategoriesByStructure([options]);
+    }
+
+    return options;
+  }),
+
 });
